b8023704843352ee26145982b75892cde3df92d2
Cédric <cedced19@gmail.com>
2016-11-12 20:21:52 +0100

    Add events to calendar

--- www/js/app.js
+++ www/js/app.js
@@ -16,20 +16,39 @@ var language = localStorage.getItem('language') || (window.navigator.userLanguag
 phonon.updateLocale(language);
 
 var dayToHour = [9, 16, 16, 13, 16, 16, 9];
-var addDays = function (days) {
+var addDays = function(days) {
     var dat = new Date();
     dat.setDate(dat.getDate() + days);
     dat.setHours(dayToHour[dat.getDay()]);
     dat.setMinutes(0);
     return dat;
 };
-var numberOfMonths = function (date1, date2) {
+var numberOfMonths = function(date1, date2) {
     var number;
-    number= (date2.getFullYear() - date1.getFullYear()) * 12;
-    number-= date1.getMonth() + 1;
-    number+= date2.getMonth() +1;
+    number = (date2.getFullYear() - date1.getFullYear()) * 12;
+    number -= date1.getMonth() + 1;
+    number += date2.getMonth() + 1;
     return number <= 0 ? 0 : number;
-}
+};
+var alertMessage = function(sentence, type) {
+    phonon.i18n().get([sentence, type, 'ok'], function(values) {
+        phonon.alert(values[sentence], values[type], false, values['ok']);
+    });
+};
+var getPermission = function(cb) {
+    cordova.plugins.permissions.hasPermission(cordova.plugins.permissions['WRITE_CALENDAR'], function(status) {
+        if (!status.hasPermission) {
+            cordova.plugins.permissions.requestPermission(cordova.plugins.permissions['WRITE_CALENDAR'], function(status) {
+                if (!status.hasPermission) return alertMessage('not_allowed_to_write_calendar', 'error');
+                cb();
+            }, function() {
+                alertMessage('not_allowed_to_write_calendar', 'error')
+            });
+        } else {
+            cb();
+        }
+    }, null);
+};
 
 phonon.navigator().on({
     page: 'home',
@@ -38,54 +57,54 @@ phonon.navigator().on({
     readyDelay: 0
 }, function(activity) {
 
-  activity.onReady(function() {
-      var ul = document.getElementById('list');
-      var lessons = JSON.parse(localStorage.getItem('lessons'));
+    activity.onReady(function() {
+        var ul = document.getElementById('list');
+        var lessons = JSON.parse(localStorage.getItem('lessons'));
 
-      while (ul.firstChild) {
-          ul.removeChild(ul.firstChild);
-      }
+        while (ul.firstChild) {
+            ul.removeChild(ul.firstChild);
+        }
 
-      if (Array.isArray(lessons)) {
-          document.getElementById('no-lesson').style.display = 'none';
-          phonon.i18n().get('available_lesson', function(value) {
-              var title = document.createElement('li');
-              title.appendChild(document.createTextNode(value));
-              title.className += 'divider';
-              ul.appendChild(title);
-              lessons.forEach(function(lesson, index) {
-                  var li = document.createElement('li');
+        if (Array.isArray(lessons)) {
+            document.getElementById('no-lesson').style.display = 'none';
+            phonon.i18n().get('available_lesson', function(value) {
+                var title = document.createElement('li');
+                title.appendChild(document.createTextNode(value));
+                title.className += 'divider';
+                ul.appendChild(title);
+                lessons.forEach(function(lesson, index) {
+                    var li = document.createElement('li');
 
-                  // Delete lesson button
-                  var deleteBtn = document.createElement('a');
-                  deleteBtn.on('click', function() {
-                      phonon.i18n().get(['question_sure', 'warning', 'ok', 'cancel'], function(values) {
-                          var confirm = phonon.confirm(values['question_sure'], values['warning'], true, values['ok'], values['cancel']);
-                          confirm.on('confirm', function() {
-                              lessons.splice(index, 1);
-                              localStorage.setItem('lessons', JSON.stringify(lessons));
-                              ul.removeChild(li);
-                          });
-                      });
-                  });
-                  deleteBtn.className += 'pull-right icon icon-close';
-                  li.appendChild(deleteBtn);
+                    // Delete lesson button
+                    var deleteBtn = document.createElement('a');
+                    deleteBtn.on('click', function() {
+                        phonon.i18n().get(['question_sure', 'warning', 'ok', 'cancel'], function(values) {
+                            var confirm = phonon.confirm(values['question_sure'], values['warning'], true, values['ok'], values['cancel']);
+                            confirm.on('confirm', function() {
+                                lessons.splice(index, 1);
+                                localStorage.setItem('lessons', JSON.stringify(lessons));
+                                ul.removeChild(li);
+                            });
+                        });
+                    });
+                    deleteBtn.className += 'pull-right icon icon-close';
+                    li.appendChild(deleteBtn);
 
-                  // Select lesson button
-                  var selectBtn = document.createElement('a');
-                  selectBtn.appendChild(document.createTextNode(lesson.name));
-                  selectBtn.on('click', function() {
+                    // Select lesson button
+                    var selectBtn = document.createElement('a');
+                    selectBtn.appendChild(document.createTextNode(lesson.name));
+                    selectBtn.on('click', function() {
 
-                  });
-                  selectBtn.className += 'padded-list';
-                  li.appendChild(selectBtn);
+                    });
+                    selectBtn.className += 'padded-list';
+                    li.appendChild(selectBtn);
 
-                  ul.appendChild(li);
-              });
-          });
-      } else {
-          document.getElementById('no-lesson').style.display = 'block';
-      }
+                    ul.appendChild(li);
+                });
+            });
+        } else {
+            document.getElementById('no-lesson').style.display = 'block';
+        }
 
     });
 });
@@ -103,50 +122,63 @@ phonon.navigator().on({
 
         submitBtn.on('click', function() {
             if (name.value == '') {
-              return phonon.i18n().get(['empty_name', 'warning', 'ok'], function(values) {
-                  phonon.alert(values['empty_name'], values['warning'], false, values['ok']);
-              });
+                return phonon.i18n().get(['empty_name', 'warning', 'ok'], function(values) {
+                    phonon.alert(values['empty_name'], values['warning'], false, values['ok']);
+                });
             }
 
             var lessons = JSON.parse(localStorage.getItem('lessons')) || [];
             var days = [3, 10];
             var nextJune = new Date();
             if (nextJune.getMonth() >= 5) {
-              nextJune.setFullYear(nextJune.getFullYear() + 1);
+                nextJune.setFullYear(nextJune.getFullYear() + 1);
             }
             nextJune.setMonth(5);
             nextJune.setDate(1);
 
-            phonon.i18n().get(['give_end_date', 'information', 'ok'], function(values) {
-                var alert = phonon.alert(values['give_end_date'], values['information'], false, values['ok']);
-                alert.on('confirm', function () {
-                    datePicker.show({
-                      date: nextJune,
-                      mode: 'date'
-                    }, function(endDate){
-                      for (var i = 0; i < numberOfMonths((new Date), endDate); i++) {
-                        days.push((i+1) * 30);
-                      }
+            getPermission(function() {
+                phonon.i18n().get(['give_end_date', 'information', 'ok', 'learn'], function(values) {
+                    var alert = phonon.alert(values['give_end_date'], values['information'], false, values['ok']);
+                    alert.on('confirm', function() {
 
-                      endDate.setHours(dayToHour[endDate.getDay()]);
-                      endDate.setMinutes(0);
+                        datePicker.show({
+                            date: nextJune,
+                            mode: 'date'
+                        }, function(endDate) {
+                            for (var i = 0; i < numberOfMonths((new Date), endDate); i++) {
+                                days.push((i + 1) * 30);
+                            }
 
-                      var lesson = {
-                        name: name.value,
-                        today: new Date(),
-                        days: days.map(addDays),
-                        end: endDate
-                      };
+                            endDate.setHours(dayToHour[endDate.getDay()]);
+                            endDate.setMinutes(0);
 
-                      if (lesson.days[lesson.days.length-1] >  endDate) {
-                        lesson.days.splice(lesson.days.length-1);
-                      }
-                      
-                      lessons.push(lesson);
-                      localStorage.setItem('lessons', JSON.stringify(lessons));
-                      name.value = '';
-                      phonon.navigator().changePage('home');
-                   });
+                            var lesson = {
+                                name: name.value,
+                                today: new Date(),
+                                days: days.map(addDays),
+                                end: endDate
+                            };
+
+                            if (lesson.days[lesson.days.length - 1] > endDate) {
+                                lesson.days.splice(lesson.days.length - 1);
+                            }
+
+                            lessons.push(lesson);
+                            localStorage.setItem('lessons', JSON.stringify(lessons));
+                            name.value = '';
+
+                            var title = values['learn'] + ' "' + lesson.name + '"';
+                            lesson.days.forEach(function(date, index) {
+                                var options = {
+                                    start: new Date(date.getFullYear(), date.getMonth(), date.getDate(), 0, 0, 0, 0, 0),
+                                    end: new Date(date.getFullYear(), date.getMonth(), date.getDate() + 1, 0, 0, 0, 0, 0)
+                                };
+                                window.plugins.calendar.createEventWithOptions(title, '', 'J' + days[index], options.start, options.end, {}, function() {}, function() {});
+                            });
+
+                            phonon.navigator().changePage('home');
+                        });
+                    });
                 });
             });
         });
